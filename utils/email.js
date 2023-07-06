da1ec169d81192b6b3b436df7fc9a42470e950da
Mohamadkazlak <mohamedkazlak2001@gmail.com>
2023-07-07 01:12:34 +0300

    first commit

---
+++ utils/email.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  // 1) Create a transporter
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: false,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  // 2) Define the email options
+  const mailOptions = {
+    from: process.env.EMAIL_USERNAME,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
+
+  // 3) Actually send the email
+  transporter.sendMail(mailOptions, (err) => {
+    if (err) {
+      console.log('it has an error', err);
+    } else {
+      console.log('email has sent' + info.response);
+    }
+  });
+};
+
+module.exports = sendEmail;
